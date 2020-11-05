ce7ada4931aee264afdfe40db9cf55e6678efb73
Suzanne Cabral <suzannecabral@live.com>
2020-11-04 20:04:08 -0800

    Find by steps works correctly

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,10 +2,15 @@
 const db = require('../data/db-config');
 
 module.exports = {
+
+//GET /api/schemes/
   find(){
     return db('schemes');
   },
 
+
+  
+// GET /api/schemes/:id
   async findById(id){
     // return db('schemes')
     //   .where('id',id);
@@ -14,12 +19,30 @@ module.exports = {
 
     if (foundScheme) {
       return db('schemes').where('id', id).first();
-    }
+    }else{
       return Promise.resolve(null);
+    }
+  },
+
+//GET /api/schemes/:id/steps
+  async findSteps(id){
+
+    const foundScheme = await db('schemes').where('id', id);
+
+    if (foundScheme) {
+      return (
+        db('steps')
+          .join('schemes', 'schemes.id', 'steps.scheme_id')
+          .where('schemes.id', id)
+          .orderBy('steps.step_number')
+      );
+    }else{
+      return Promise.resolve(null);
+    }
   },
 
 
-  findSteps(id){},
+
   add(scheme){},
   update(changes,id){},
   remove(id){}
