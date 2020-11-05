d5fb942dc0a1570c5d4202148a2e83310d5f2f36
Suzanne Cabral <suzannecabral@live.com>
2020-11-04 18:53:10 -0800

    added placeholder methods

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -4,5 +4,11 @@ const db = require('../data/db-config');
 module.exports = {
   find(){
     return db('schemes');
-  }
+  },
+  findById(id){},
+  findSteps(id){},
+  add(scheme){},
+  update(changes,id){},
+  remove(id){}
+  
 };
