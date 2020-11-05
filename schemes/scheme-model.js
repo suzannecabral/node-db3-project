e3f512c158e2a8e761fef7e0f89c6ac4488b59d0
Suzanne Cabral <suzannecabral@live.com>
2020-11-04 19:27:26 -0800

    Find by id works correctly

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -5,7 +5,20 @@ module.exports = {
   find(){
     return db('schemes');
   },
-  findById(id){},
+
+  async findById(id){
+    // return db('schemes')
+    //   .where('id',id);
+
+    const foundScheme = await db('schemes').where('id', id);
+
+    if (foundScheme) {
+      return db('schemes').where('id', id).first();
+    }
+      return Promise.resolve(null);
+  },
+
+
   findSteps(id){},
   add(scheme){},
   update(changes,id){},
