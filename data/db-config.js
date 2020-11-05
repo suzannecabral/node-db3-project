99ee8e86a4fb5cfb62c68d3d0b31c212b5726c0e
Suzanne Cabral <suzannecabral@live.com>
2020-11-04 18:50:15 -0800

    Knex configured, find all works

--- data/db-config.js
+++ data/db-config.js
@@ -1,1 +1,5 @@
 // db-config
+const knex = require('knex');
+const configs = require('../knexfile');
+
+module.exports = knex(configs.development);
--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,8 @@
 // scheme-model
+const db = require('../data/db-config');
+
+module.exports = {
+  find(){
+    return db('schemes');
+  }
+};
